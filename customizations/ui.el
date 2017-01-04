11f179c300b10a067c9eac9013be037218c52ae2
Abdussalam Abdurrahman <abdusalam@live.com>
2017-01-03 17:58:12 -0800

    if -> when

--- customizations/ui.el
+++ customizations/ui.el
@@ -7,7 +7,8 @@
 ;; Turn off the menu bar at the top of each frame because it's distracting,
 ;; unless it's on macOS where the single menu bar is always present but with
 ;; -1 will not include the mode-specific menu entries.
-(if (not (eq system-type 'darwin)) (menu-bar-mode -1))
+(when (not (eq system-type 'darwin))
+  (menu-bar-mode -1))
 
 ;; Show line numbers
 (global-linum-mode)
