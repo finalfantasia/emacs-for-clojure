e307ec41f1e3bfdbcb0922fde13a09f01b5ea4f5
Abdussalam Abdurrahman <abdusalam@live.com>
2017-01-03 18:03:59 -0800

    remove obsolete configurations.

--- customizations/ui.el
+++ customizations/ui.el
@@ -46,12 +46,7 @@
 ;; (setq initial-frame-alist '((top . 0) (left . 0) (width . 177) (height . 53)))
 
 ;; These settings relate to how emacs interacts with your operating system
-(setq ;; makes killing/yanking interact with the clipboard
-      x-select-enable-clipboard t
-
-      ;; I'm actually not sure what this does but it's recommended?
-      x-select-enable-primary t
-
+(setq
       ;; Save clipboard strings into kill ring before replacing them.
       ;; When one selects something in another program to paste it into Emacs,
       ;; but kills something in Emacs before actually pasting it,
